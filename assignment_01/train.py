baa3f10e2e8ccdfa699a2090c83cb9553bda0b28
Moses A Gordon <mosesgordon@brandeis.edu>
2024-02-07 19:01:52 -0500

    eval function

--- assignment_01/train.py
+++ assignment_01/train.py
@@ -55,8 +55,20 @@ class Trainer:
 
     def eval(self, test_loader: DataLoader) -> float:
         """ Model evaluation, return the model accuracy over test set """
+        print("Start testing...")
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for data in test_loader:
+                images, labels = data
+                outputs = self._model(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
 
-        self._model.eval()
+        print('Accuracy of the network on the 10000 test images: %d %%' % (
+            100 * correct / total))
+ ##       self._model.eval()
         return
 
     def infer(self, sample: Tensor) -> int:
